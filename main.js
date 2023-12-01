c88cbb4fb3cd7d422113849193713b8c419e9247
Samuel Legrain <samuel.legrain@bstorm.be>
2023-12-01 14:02:22 +0100

    fetch() js

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-const main = document.querySelector('main');
+( async function(){
+    const main = document.querySelector('main');
 
-const getData = function(url, callback, options = {method : 'GET', async : true, body : null}){
+    const getData = function(url, callback, options = {method : 'GET', async : true, body : null}){
 
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function(){
-        if(this.readyState == 4){
-            console.log(`Status HTTP : ${xhr.status}`);
-            if (xhr.status == 200 || xhr.status == 0) {
-                let data = JSON.parse(xhr.responseText);
-                callback(data);
-            }
+    if(this.readyState == 4){
+        console.log(`Status HTTP : ${xhr.status}`);
+        if (xhr.status == 200 || xhr.status == 0) {
+            let data = JSON.parse(xhr.responseText);
+            callback(data);
         }
+    }
     } ;
 
     xhr.open(options.method,
-        url,
-        options.async);
+    url,
+    options.async);
 
     xhr.send(options.body);
-}
+    }
 
-const generateCard = function(data){
+    const generateCard = function(data){
     let cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
     main.appendChild(cardDiv);
@@ -37,22 +38,59 @@ const generateCard = function(data){
     cardDiv.appendChild(birthdateP);
     let addressAddress = document.createElement('address');
     addressAddress.innerHTML = `${data.location.street.name} ${data.location.street.number}`
-                                + `<br>${data.location.postcode} ${data.location.city}`
-                                + `<br>${data.location.country.toUpperCase()}`;
+                            + `<br>${data.location.postcode} ${data.location.city}`
+                            + `<br>${data.location.country.toUpperCase()}`;
     cardDiv.appendChild(addressAddress);
-}
-
-const personsHandler = function(data){
-    for (const person of data.results) {
-        generateCard(person);
     }
-}
 
-getData(
+    const personsHandler = function(data){
+    for (const person of data.results) {
+    generateCard(person);
+    }
+    }
+
+    //Soit via XHR :
+    /*
+    getData(
     'https://randomuser.me/api/?nat=fr&results=5',  //URL :    adresse permettant d'avoir accès au service API
     personsHandler,                                 //CALLBACK :    fonction exécuté lors de la récupération
-                                                    //              des données
-    {method : "GET", body : null, async : false}    //OPTIONS :     objet JS avec les différentes options
-                                                    //(optionnel)   nécessaires (method, async, body, ...)
+                                                //              des données
+    {method : "GET", body : null, async : true}    //OPTIONS :     objet JS avec les différentes options
+                                                //(optionnel)   nécessaires (method, async, body, ...)
     );
+    */
+    //Soit via le fetch().then() avec fonction async:
+    /*
+    fetch('https://randomuser.me/api/?nat=fr&results=5').then(
+        async function (reponse){
+            personsHandler(await reponse.json());
+        }
+    );
+    */
 
+    //Soit via le fetch() avec variables intermédiaires :
+    /*
+    let reponse = await fetch('https://randomuser.me/api/?nat=fr&results=5');
+    let json = await reponse.json();
+    personsHandler(json);
+    */
+    //Soit via le fetch().then() avec .then() sur la reponse:
+    fetch('https://randomuser.me/api/?nat=fr&results=5')
+        .then(reponse => reponse.json()
+                                    .then(data => personsHandler(data)));
+})();
+
+//Exemple différence entre fonction synchrone et asynchrone
+/*
+const functionSynchrone = function(){
+    return "toto";
+}
+
+const functionAsynchrone = async function(){
+    return "toto";
+}
+
+let syncValue = functionSynchrone();
+
+let asyncValue = await functionAsynchrone();
+*/
